51fbf9f37503e872d9da9208e8dec735aa3cad06
RuGe <70239531+ruge0326@users.noreply.github.com>
2022-12-11 20:27:02 -0600

    Init project

---
+++ app/page.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className="py-6">
+      <div className="flex bg-white rounded-lg shadow-lg overflow-hidden mx-auto max-w-sm lg:max-w-4xl">
+        <div
+          className="hidden lg:block lg:w-1/2 bg-cover"
+          style={{
+            backgroundImage: `url('https://images.unsplash.com/photo-1546514714-df0ccc50d7bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80')`,
+          }}
+        ></div>
+        <div className="w-full p-8 lg:w-1/2">
+          <h2 className="text-2xl font-semibold text-gray-700 text-center">
+            Nfit
+          </h2>
+          <p className="text-xl text-gray-600 text-center">Bienvenido!</p>
+          <div className="mt-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Usuario
+            </label>
+            <input
+              className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
+              type="text"
+            />
+          </div>
+          <div className="mt-4">
+            <div className="flex justify-between">
+              <label className="block text-gray-700 text-sm font-bold mb-2">
+                Contraseña
+              </label>
+            </div>
+            <input
+              className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
+              type="password"
+            />
+          </div>
+          <div className="mt-8">
+            <button className="bg-gray-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-gray-600">
+              Ingresar
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
